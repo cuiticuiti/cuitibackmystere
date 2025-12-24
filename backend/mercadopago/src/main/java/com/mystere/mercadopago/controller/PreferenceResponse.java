1a44d20e4a3fa0995926dcca0e5ab95d58a8c5ae
cuiticuiti <nico123.nco@gmail.com>
2025-12-24 00:28:39 -0300

    Update PreferenceResponse.java

--- backend/mercadopago/src/main/java/com/mystere/mercadopago/controller/PreferenceResponse.java
+++ backend/mercadopago/src/main/java/com/mystere/mercadopago/controller/PreferenceResponse.java
@@ -1,6 +1,24 @@
 package com.mystere.mercadopago.controller;
 
-public record PreferenceResponse(
-        String id,
-        String init_point
-) {}
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class PreferenceResponse {
+
+    private String id;
+
+    @JsonProperty("initPoint")
+    private String initPoint;
+
+    public PreferenceResponse(String id, String initPoint) {
+        this.id = id;
+        this.initPoint = initPoint;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getInitPoint() {
+        return initPoint;
+    }
+}
